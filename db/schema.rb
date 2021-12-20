963723d22c322fb5b35076b2073f36f8621e3dcd
Fajarullah <frullah12@gmail.com>
2021-12-20 17:58:59 +0700

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_161951) do
+ActiveRecord::Schema.define(version: 2021_12_20_104705) do
 
   create_table "notifications", force: :cascade do |t|
     t.string "message"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2021_12_19_161951) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
